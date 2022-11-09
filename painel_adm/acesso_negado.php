0030c303a50b344f2fbea6f29995d4c8edd0c9ec
Otávio Montalvão <om16design@gmail.com>
2022-11-09 17:49:43 -0300

    dadasdsa

---
+++ painel_adm/acesso_negado.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title> Acesso Negado </title>
+</head>
+<style>
+  .error {
+    background-color: #fce4e4;
+    border: 1px solid #fcc2c3;
+    float: left;
+    padding: 20px 30px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    font-weight: 100;
+  }
+
+  a {
+    padding-left: 30px;
+  }
+
+  i {
+    color: black;
+  }
+  i:hover {
+    color: gray;
+  }
+</style>
+
+<body>
+
+  <h1 class="error"> Usuário ou senha inválida! <a href="index.php"> <i class="fa-solid fa-backward"></i> </a> </h1>
+
+</body>
+
+</html>
